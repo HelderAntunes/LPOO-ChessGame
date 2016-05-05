a14a71ca5d78311f39146572db10f32d4668d20e
HelderAntunes <helder_antunes-@hotmail.com>
2016-05-05 16:31:51 +0100

    Introducao de turnos no jogo

--- src/chessGame/logic/ChessGame.java
+++ src/chessGame/logic/ChessGame.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 public class ChessGame {
 	
 	private Board board;
-	private Player playerWhite;
-	private Player playerBlack;
+	private Player playerWhite = new Player(Color.WHITE);
+	private Player playerBlack = new Player(Color.BLACK);
+	private boolean isWhiteToPlay = true;
 	
 	
 	public ChessGame(char[][] board){
 		this.board = new Board(board);
-		this.playerBlack = new Player(Color.BLACK);
-		this.playerWhite = new Player(Color.WHITE);
 	}
 	
 	public ArrayList<Position> getPossibleMoves(Position position){
@@ -24,26 +23,28 @@ public class ChessGame {
 	 * The function return false when there isn't a piece in initial position, 
 	 * the player try move a opponent piece, or the move is invalid
 	 * 
-	 * @param player
 	 * @param iniPosition initial position
 	 * @param endPosition end position
 	 * @return true if the move was successful , false otherwise.
 	 */
-	public boolean move(Player player, Position iniPosition, Position endPosition){
+	public boolean move(Position iniPosition, Position endPosition){
 		
 		if(board.getBox(iniPosition).isEmpty() == true)
 			return false;
 		
-		if(player.getColor() != board.getBox(iniPosition).getPiece().getColor())
+		if(isWhiteToPlay == true && board.getBox(iniPosition).getPiece().getColor() == Color.BLACK)
+			return false;
+		
+		if(isWhiteToPlay == false && board.getBox(iniPosition).getPiece().getColor() == Color.WHITE)
 			return false;
 		
 		ArrayList<Position> possibleMoves = getPossibleMoves(iniPosition);
-		for(int i = 0;i < possibleMoves.size();i++){
+		for(int i = 0;i < possibleMoves.size();i++)
 			if(possibleMoves.get(i).equals(endPosition)){
 				board.move(iniPosition, endPosition);
+				isWhiteToPlay = !isWhiteToPlay;
 				return true;
 			}
-		}
 		
 		return false;
 	}
@@ -51,4 +52,5 @@ public class ChessGame {
 	public char[][] getBoard(){
 		return board.getBoard();
 	}
+
 }
