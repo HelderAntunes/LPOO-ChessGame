b15637974b3fb84541fa7fceba9fe21fa4ff13fd
HelderAntunes <helder_antunes-@hotmail.com>
2016-05-05 14:44:15 +0100

    Movimento do rei

--- src/chessGame/logic/King.java
+++ src/chessGame/logic/King.java
@@ -18,6 +18,45 @@ class King extends Piece{
 	
 	ArrayList<Position> getPossibleMoves(Box board[][]){
 		ArrayList<Position> validPositions = new ArrayList<Position>();
+		
+		int x = this.position.getX();
+		int y = this.position.getY();
+		
+		if(y >= 1){
+			if(x >= 1){
+				validPositions.add(new Position(x-1, y-1));
+			}
+			if(x <= 6){
+				validPositions.add(new Position(x+1, y-1));
+			}
+			validPositions.add(new Position(x, y-1));
+		}
+		if(x >= 1){
+			validPositions.add(new Position(x-1, y));
+		}
+		if(x <= 6){
+			validPositions.add(new Position(x+1, y));
+		}
+		if(y <= 6){
+			if(x >= 1){
+				validPositions.add(new Position(x-1, y+1));
+			}
+			if(x <= 6){
+				validPositions.add(new Position(x+1, y+1));
+			}
+			validPositions.add(new Position(x, y+1));
+		}
+		
+		for(int i = 0;i < validPositions.size();i++){
+			Position position = validPositions.get(i);
+			int yp = position.getY();
+			int xp = position.getX();
+			if(board[yp][xp].isEmpty() == false && board[yp][xp].getPiece().getColor() == this.getColor()){
+				validPositions.remove(i);
+				i--;
+			}
+		}
+		
 		return validPositions;
 	}
 }
